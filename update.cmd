a53f693f82f13877a3dd71538054497e4ae64b30
r3adm3 <adrian@nofishhere.com>
2012-11-20 12:56:24 +0000

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -4,18 +4,32 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET IISLOGSFOLDER=C:\INETPUB\LOGS
 
+REM ****************************************************************************
+REM  Set the source directory for the distribution, by extracting it from a CD 
+REM  command
+REM ****************************************************************************
+REM FOR /F "delims=" %%I IN ('ECHO %CD%') DO SET SOURCE=%%I
+SET SOURCE=E:\PERFLOGS
+ECHO Setting SOURCE to %SOURCE%
+
+REM ****************************************************************************
+REM  Need a version of the source variable with an extra esc character
+REM ****************************************************************************
+SET GRAPHSOURCE=%SOURCE::=\:%
+
+
 REM ***************************************************************************
 REM  Summarising Data from Eventlogs from last 5 minutes. 
 REM ***************************************************************************
-
-e:\schedjobs\eventlogs\tools\logparser "select EventType, count(*) as num from Application where timeWritten > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 00:05', 'yyyy-MM-dd HH:mm')) group by EventType" -q -iCheckpoint:e:\schedjobs\eventlogs\chkpts\app.lpc> e:\schedjobs\eventlogs\AppSum.txt
-e:\schedjobs\eventlogs\tools\logparser "select EventType, count(*) as num from Security where timeWritten > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 00:05', 'yyyy-MM-dd HH:mm')) group by EventType" -q -iCheckpoint:e:\schedjobs\eventlogs\chkpts\sec.lpc> e:\schedjobs\eventlogs\SecSum.txt
-e:\schedjobs\eventlogs\tools\logparser "select EventType, count(*) as num from System where timeWritten > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 00:05', 'yyyy-MM-dd HH:mm')) group by EventType" -q -iCheckpoint:e:\schedjobs\eventlogs\chkpts\sys.lpc> e:\schedjobs\eventlogs\SysSum.txt
+ECHO  - Collecting Data...
+%SOURCE%\tools\logparser "select EventType, count(*) as num from Application where timeWritten > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 00:05', 'yyyy-MM-dd HH:mm')) group by EventType" -q -iCheckpoint:%SOURCE%\chkpts\app.lpc> %SOURCE%\AppSum.txt
+%SOURCE%\tools\logparser "select EventType, count(*) as num from Security where timeWritten > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 00:05', 'yyyy-MM-dd HH:mm')) group by EventType" -q -iCheckpoint:%SOURCE%\chkpts\sec.lpc> %SOURCE%\SecSum.txt
+%SOURCE%\tools\logparser "select EventType, count(*) as num from System where timeWritten > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 00:05', 'yyyy-MM-dd HH:mm')) group by EventType" -q -iCheckpoint:%SOURCE%\chkpts\sys.lpc> %SOURCE%\SysSum.txt
 
 REM ***************************************************************************
 REM  Summarising Data from WMI 
 REM ***************************************************************************
-cscript //nologo e:\schedjobs\eventlogs\tools\GetWMICounters.vbs > WMIStats.txt
+cscript //nologo %SOURCE%\tools\GetWMICounters.vbs > %SOURCE%\WMIStats.txt
 
 GOTO SKIPIIS
 
@@ -30,10 +44,10 @@ GOTO SKIPIIS
 	 SET YEAR=!YEAR:~2,2!
 	)
 	ECHO %YEAR%%MONTH%%DAY%
-	e:\schedjobs\eventlogs\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status < 299" -recurse -iCheckpoint:chkpts\200s.lpc -q >200s.txt
-	e:\schedjobs\eventlogs\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status between 300 and 399" -recurse -iCheckpoint:chkpts\300s.lpc -q >300s.txt
-	e:\schedjobs\eventlogs\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status between 400 and 499" -recurse -iCheckpoint:chkpts\400s.lpc -q >400s.txt
-	e:\schedjobs\eventlogs\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status > 499" -recurse -iCheckpoint:chkpts\200s.lpc -q >500s.txt
+	%SOURCE%\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status < 299" -recurse -iCheckpoint:%SOURCE%chkpts\200s.lpc -q >200s.txt
+	%SOURCE%\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status between 300 and 399" -recurse -iCheckpoint:%SOURCE%chkpts\300s.lpc -q >300s.txt
+	%SOURCE%\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status between 400 and 499" -recurse -iCheckpoint:%SOURCE%chkpts\400s.lpc -q >400s.txt
+	%SOURCE%\tools\logparser -i:iisw3c "select count(*) as num from %IISLOGSFOLDER%\*%YEAR%%MONTH%%DAY%.log where time > SUB(TO_LOCALTIME(SYSTEM_TIMESTAMP()), TIMESTAMP('0000-01-01 23:05', 'yyyy-MM-dd HH:mm')) and sc-status > 499" -recurse -iCheckpoint:%SOURCE%chkpts\200s.lpc -q >500s.txt
 	GOTO END
 
 :SKIPIIS
@@ -58,7 +72,7 @@ SET DSKAVGBYTESWRITE=0
 SET MEMCOMMITTEDBYTES=0
 SET MEMPAGESPERSEC=0
 SET MEMAVAILMBYTES=0
-FOR /F "DELIMS=: tokens=1-5" %%i IN (WMISTATS.TXT) DO (
+FOR /F "DELIMS=: tokens=1-5" %%i IN (%SOURCE%\WMISTATS.TXT) DO (
  IF %%i==Network SET BYTESIN=%%j
  IF %%i==Network SET BYTESOUT=%%k
  IF %%i==Network SET BYTESTOTAL=%%l
@@ -77,11 +91,11 @@ FOR /F "DELIMS=: tokens=1-5" %%i IN (WMISTATS.TXT) DO (
  IF %%i==MemoryUsage SET MEMPAGESPERSEC=%%k
  IF %%i==MemoryUsage SET MEMAVAILMBYTES=%%l
 )
-ECHO %BYTESIN%:%BYTESOUT%:%BYTESTOTAL% >>log.txt
-ECHO %CPUPRIV%:%CPUUSER%:%CPUTOTAL% >>log.txt
-ECHO %DRVCSPACE%:%DRVDSPACE%:%DRVESPACE% >>log.txt
-ECHO %DSKAVGQTOTAL%:%DSKAVGQREAD%:%DSKAVGQWRITE%:%DSKAVGBYTESREAD%:%DSKAVGBYTESWRITE% >>log.txt
-ECHO %MEMCOMMITTEDBYTES%:%MEMPAGESPERSEC%:%MEMAVAILMBYTES% >>log.txt
+ECHO %BYTESIN%:%BYTESOUT%:%BYTESTOTAL% >%SOURCE%\log.txt
+ECHO %CPUPRIV%:%CPUUSER%:%CPUTOTAL% >>%SOURCE%\log.txt
+ECHO %DRVCSPACE%:%DRVDSPACE%:%DRVESPACE% >>%SOURCE%\log.txt
+ECHO %DSKAVGQTOTAL%:%DSKAVGQREAD%:%DSKAVGQWRITE%:%DSKAVGBYTESREAD%:%DSKAVGBYTESWRITE% >>%SOURCE%\log.txt
+ECHO %MEMCOMMITTEDBYTES%:%MEMPAGESPERSEC%:%MEMAVAILMBYTES% >>%SOURCE%\log.txt
 
 REM ***************************************************************************
 REM Analyse AppSum, stick data into Variables so that we can make use of them 
@@ -93,7 +107,7 @@ SET APPERR=0
 SET APPAUDITFAIL=0
 SET APPAUDITSUCCESS=0
 SET APPSUCCESS=0
-FOR /F "tokens=1,2" %%i in (appsum.txt) DO (
+FOR /F "tokens=1,2" %%i in (%SOURCE%\appsum.txt) DO (
   IF %%i==0 SET APPSUCCESS=%%j
   IF %%i==1 SET APPERR=%%j
   IF %%i==2 SET APPWARN=%%j
@@ -102,7 +116,7 @@ FOR /F "tokens=1,2" %%i in (appsum.txt) DO (
   IF %%i==16 SET APPAUDITFAIL=%%j
 )
 
-ECHO %APPSUCCESS%:%APPERR%:%APPWARN%:%APPINFO%:%APPAUDITSUCCESS%:%APPAUDITFAIL% >>log.txt
+ECHO %APPSUCCESS%:%APPERR%:%APPWARN%:%APPINFO%:%APPAUDITSUCCESS%:%APPAUDITFAIL% >>%SOURCE%\log.txt
 
 REM ***************************************************************************
 REM Analyse SecSum, stick data into Variables so that we can make use of them 
@@ -114,7 +128,7 @@ SET SecERR=0
 SET SecAUDITFAIL=0
 SET SecAUDITSUCCESS=0
 SET SecSUCCESS=0
-FOR /F "tokens=1,2" %%i in (Secsum.txt) DO (
+FOR /F "tokens=1,2" %%i in (%SOURCE%\Secsum.txt) DO (
   IF %%i==0 SET SecSUCCESS=%%j
   IF %%i==1 SET SecERR=%%j
   IF %%i==2 SET SecWARN=%%j
@@ -122,7 +136,7 @@ FOR /F "tokens=1,2" %%i in (Secsum.txt) DO (
   IF %%i==8 SET SecAUDITSUCCESS=%%j
   IF %%i==16 SET SecAUDITFAIL=%%j
 )
-ECHO %SecSUCCESS%:%SecERR%:%SecWARN%:%SecINFO%:%SecAUDITSUCCESS%:%SecAUDITFAIL% >>log.txt
+ECHO %SecSUCCESS%:%SecERR%:%SecWARN%:%SecINFO%:%SecAUDITSUCCESS%:%SecAUDITFAIL% >>%SOURCE%\log.txt
 
 REM ***************************************************************************
 REM Analyse SysSum, stick data into Variables so that we can make use of them 
@@ -142,50 +156,52 @@ FOR /F "tokens=1,2" %%i in (Syssum.txt) DO (
   IF %%i==8 SET SysAUDITSUCCESS=%%j
   IF %%i==16 SET SysAUDITFAIL=%%j
 )
-ECHO %SysSUCCESS%:%SysERR%:%SysWARN%:%SysINFO%:%SysAUDITSUCCESS%:%SysAUDITFAIL% >>log.txt
+ECHO %SysSUCCESS%:%SysERR%:%SysWARN%:%SysINFO%:%SysAUDITSUCCESS%:%SysAUDITFAIL% >>%SOURCE%\log.txt
 
 REM ***************************************************************************
 REM Update event log rrd's
 REM ***************************************************************************
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\AppEvents.rrd N:%APPSUCCESS%:%APPERR%:%APPWARN%:%APPINFO%:%APPAUDITSUCCESS%:%APPAUDITFAIL%
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\SecEvents.rrd N:%SecSUCCESS%:%SecERR%:%SecWARN%:%SecINFO%:%SecAUDITSUCCESS%:%SecAUDITFAIL%
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\SysEvents.rrd N:%SysSUCCESS%:%SysERR%:%SysWARN%:%SysINFO%:%SysAUDITSUCCESS%:%SysAUDITFAIL%
+ECHO  - Updating Rrds ...
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\AppEvents.rrd N:%APPSUCCESS%:%APPERR%:%APPWARN%:%APPINFO%:%APPAUDITSUCCESS%:%APPAUDITFAIL%
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\SecEvents.rrd N:%SecSUCCESS%:%SecERR%:%SecWARN%:%SecINFO%:%SecAUDITSUCCESS%:%SecAUDITFAIL%
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\SysEvents.rrd N:%SysSUCCESS%:%SysERR%:%SysWARN%:%SysINFO%:%SysAUDITSUCCESS%:%SysAUDITFAIL%
 
 REM ***************************************************************************
 REM Update Network log rrd's
 REM ***************************************************************************
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\Network.rrd N:%BYTESIN%:%BYTESOUT%:%BYTESTOTAL%
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\Network.rrd N:%BYTESIN%:%BYTESOUT%:%BYTESTOTAL%
 
 REM ***************************************************************************
 REM Update CPU log rrd's
 REM ***************************************************************************
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\CPU.rrd N:%CPUTOTAL%:%CPUUSER%:%CPUPRIV%
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\CPU.rrd N:%CPUPRIV%:%CPUUSER%:%CPUTOTAL%
 
 REM ***************************************************************************
 REM Update DiskSpace log rrd's
 REM ***************************************************************************
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\DriveSpace.rrd N:%DRVCSPACE%:%DRVDSPACE%:%DRVESPACE% 
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\DriveSpace.rrd N:%DRVCSPACE%:%DRVDSPACE%:%DRVESPACE% 
 
 REM ***************************************************************************
 REM Update DiskUsage log rrd's
 REM ***************************************************************************
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\DriveUsage.rrd N:%DSKAVGQTOTAL%:%DSKAVGQREAD%:%DSKAVGQWRITE%:%DSKAVGBYTESREAD%:%DSKAVGBYTESWRITE%
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\DriveUsage.rrd N:%DSKAVGQTOTAL%:%DSKAVGQREAD%:%DSKAVGQWRITE%:%DSKAVGBYTESREAD%:%DSKAVGBYTESWRITE%
 
 REM ***************************************************************************
 REM Update MemoryUsage log rrd's
 REM ***************************************************************************
-E:\schedjobs\eventlogs\tools\rrdtool.exe update E:\schedjobs\eventlogs\rrds\Memory.rrd N:%MEMCOMMITTEDBYTES%:%MEMPAGESPERSEC%:%MEMAVAILMBYTES%
+%SOURCE%\tools\rrdtool.exe update %SOURCE%\rrds\Memory.rrd N:%MEMCOMMITTEDBYTES%:%MEMPAGESPERSEC%:%MEMAVAILMBYTES%
 
 REM ***************************************************************************
 REM Draw MemoryUsage log graphs
 REM ***************************************************************************
-e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\AppEvents.png --start -86400 -E^
- DEF:AppInfo=rrds\AppEvents.rrd:AppInfo:AVERAGE^
- DEF:AppWarn=rrds\AppEvents.rrd:AppWarn:AVERAGE^
- DEF:AppErr=rrds\AppEvents.rrd:AppErr:AVERAGE^
- DEF:AppAuditFail=rrds\AppEvents.rrd:AppAuditFail:AVERAGE^
- DEF:AppAuditSuccess=rrds\AppEvents.rrd:AppAuditSuccess:AVERAGE^
- DEF:AppSuccess=rrds\AppEvents.rrd:AppSuccess:AVERAGE^
+ECHO  - Drawing Graphs ...
+%SOURCE%\tools\rrdtool.exe graph %SOURCE%\pngs\AppEvents.png --start -86400 -E^
+ DEF:AppInfo=%GRAPHSOURCE%\rrds\AppEvents.rrd:AppInfo:AVERAGE^
+ DEF:AppWarn=%GRAPHSOURCE%\rrds\AppEvents.rrd:AppWarn:AVERAGE^
+ DEF:AppErr=%GRAPHSOURCE%\rrds\AppEvents.rrd:AppErr:AVERAGE^
+ DEF:AppAuditFail=%GRAPHSOURCE%\rrds\AppEvents.rrd:AppAuditFail:AVERAGE^
+ DEF:AppAuditSuccess=%GRAPHSOURCE%\rrds\AppEvents.rrd:AppAuditSuccess:AVERAGE^
+ DEF:AppSuccess=%GRAPHSOURCE%\rrds\AppEvents.rrd:AppSuccess:AVERAGE^
  AREA:AppInfo#0000FF88:Infos^
  LINE1:AppWarn#FCB514:Warning^
  LINE1:AppErr#FF0000:Errors^
@@ -193,13 +209,13 @@ e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\AppEv
  LINE1:AppAuditSuccess#8B1C62:AuditSuccess^
  LINE1:AppSuccess#00FF00:Success
 
-e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\SysEvents.png --start -86400 -E^
- DEF:SysInfo=rrds\SysEvents.rrd:SysInfo:AVERAGE^
- DEF:SysWarn=rrds\SysEvents.rrd:SysWarn:AVERAGE^
- DEF:SysErr=rrds\SysEvents.rrd:SysErr:AVERAGE^
- DEF:SysAuditFail=rrds\SysEvents.rrd:SysAuditFail:AVERAGE^
- DEF:SysAuditSuccess=rrds\SysEvents.rrd:SysAuditSuccess:AVERAGE^
- DEF:SysSuccess=rrds\SysEvents.rrd:SysSuccess:AVERAGE^
+%SOURCE%\tools\rrdtool.exe graph %SOURCE%\pngs\SysEvents.png --start -86400 -E^
+ DEF:SysInfo=%GRAPHSOURCE%\rrds\SysEvents.rrd:SysInfo:AVERAGE^
+ DEF:SysWarn=%GRAPHSOURCE%\rrds\SysEvents.rrd:SysWarn:AVERAGE^
+ DEF:SysErr=%GRAPHSOURCE%\rrds\SysEvents.rrd:SysErr:AVERAGE^
+ DEF:SysAuditFail=%GRAPHSOURCE%\rrds\SysEvents.rrd:SysAuditFail:AVERAGE^
+ DEF:SysAuditSuccess=%GRAPHSOURCE%\rrds\SysEvents.rrd:SysAuditSuccess:AVERAGE^
+ DEF:SysSuccess=%GRAPHSOURCE%\rrds\SysEvents.rrd:SysSuccess:AVERAGE^
  AREA:SysInfo#0000FF88:Infos^
  LINE1:SysWarn#FCB514:Warning^
  LINE1:SysErr#FF0000:Errors^
@@ -207,13 +223,13 @@ e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\SysEv
  LINE1:SysAuditSuccess#8B1C62:AuditSuccess^
  LINE1:SysSuccess#00FF00:Success
 
-e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\SecEvents.png --start -86400 -E^
- DEF:SecInfo=rrds\SecEvents.rrd:SecInfo:AVERAGE^
- DEF:SecWarn=rrds\SecEvents.rrd:SecWarn:AVERAGE^
- DEF:SecErr=rrds\SecEvents.rrd:SecErr:AVERAGE^
- DEF:SecAuditFail=rrds\SecEvents.rrd:SecAuditFail:AVERAGE^
- DEF:SecAuditSuccess=rrds\SecEvents.rrd:SecAuditSuccess:AVERAGE^
- DEF:SecSuccess=rrds\SecEvents.rrd:SecSuccess:AVERAGE^
+%SOURCE%\tools\rrdtool.exe graph %SOURCE%\pngs\SecEvents.png --start -86400 -E^
+ DEF:SecInfo=%GRAPHSOURCE%\rrds\SecEvents.rrd:SecInfo:AVERAGE^
+ DEF:SecWarn=%GRAPHSOURCE%\rrds\SecEvents.rrd:SecWarn:AVERAGE^
+ DEF:SecErr=%GRAPHSOURCE%\rrds\SecEvents.rrd:SecErr:AVERAGE^
+ DEF:SecAuditFail=%GRAPHSOURCE%\rrds\SecEvents.rrd:SecAuditFail:AVERAGE^
+ DEF:SecAuditSuccess=%GRAPHSOURCE%\rrds\SecEvents.rrd:SecAuditSuccess:AVERAGE^
+ DEF:SecSuccess=%GRAPHSOURCE%\rrds\SecEvents.rrd:SecSuccess:AVERAGE^
  AREA:SecInfo#0000FF88:Infos^
  LINE1:SecWarn#FCB514:Warning^
  LINE1:SecErr#FF0000:Errors^
@@ -221,19 +237,20 @@ e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\SecEv
  LINE1:SecAuditSuccess#8B1C62:AuditSuccess^
  LINE1:SecSuccess#00FF00:Success
 
- e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\Network.png --start -86400 -E^
- DEF:BytesIn=rrds\Network.rrd:BytesIn:AVERAGE^
- DEF:BytesOut=rrds\Network.rrd:BytesOut:AVERAGE^
- DEF:BytesTotal=rrds\Network.rrd:BytesTotal:AVERAGE^
+ %SOURCE%\tools\rrdtool.exe graph %SOURCE%\pngs\Network.png --start -86400 -E^
+ DEF:BytesIn=%GRAPHSOURCE%\rrds\Network.rrd:BytesIn:AVERAGE^
+ DEF:BytesOut=%GRAPHSOURCE%\rrds\Network.rrd:BytesOut:AVERAGE^
+ DEF:BytesTotal=%GRAPHSOURCE%\rrds\Network.rrd:BytesTotal:AVERAGE^
  LINE1:BytesIn#0000FF88:BytesIn^
  LINE1:BytesOut#FCB514:BytesOut^
  LINE1:BytesTotal#00FF00:BytesTotal
 
-e:\schedjobs\eventlogs\tools\rrdtool.exe graph e:\schedjobs\eventlogs\pngs\CPU.png --start -86400 -E^
- DEF:CpuPriv=rrds\CPU.rrd:CpuPriv:AVERAGE^
- DEF:CpuUser=rrds\CPU.rrd:CpuUser:AVERAGE^
- DEF:CpuTotal=rrds\CPU.rrd:CpuTotal:AVERAGE^
+%SOURCE%\tools\rrdtool.exe graph %SOURCE%\pngs\CPU.png --start -86400 -E^
+ DEF:CpuPriv=%GRAPHSOURCE%\rrds\CPU.rrd:CpuPriv:AVERAGE^
+ DEF:CpuUser=%GRAPHSOURCE%\rrds\CPU.rrd:CpuUser:AVERAGE^
+ DEF:CpuTotal=%GRAPHSOURCE%\rrds\CPU.rrd:CpuTotal:AVERAGE^
  LINE1:CpuPriv#0000FF88:CpuPriv^
  LINE1:CpuUser#FCB514:CpuUser^
  LINE1:CpuTotal#FF0000:CpuTotal
- :END
+
+:END
